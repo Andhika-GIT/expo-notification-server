6b930f842275b17c77d6ee3b5e06058b7af36d8c
Andhika Prasetya <andhikapr081@gmail.com>
2023-09-20 12:59:35 +0700

    initial

---
+++ index.js
@@ -0,0 +1,25 @@
+import { Expo } from "expo-server-sdk";
+
+const expo = new Expo();
+
+const TOKEN = "test";
+
+const sendNotification = async (token) => {
+  if (Expo.isExpoPushToken(token)) {
+    const notificationResponse = await expo.sendPushNotificationsAsync([
+      {
+        to: token,
+        title: "hello !",
+        body: "this is expo notification",
+        data: {
+          greeting: "greeting!",
+        },
+      },
+    ]);
+    console.log("notification send : ", notificationResponse);
+  } else {
+    console.log(`${token} this is not an expo token`);
+  }
+};
+
+sendNotification(TOKEN);
